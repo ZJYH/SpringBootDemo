696a729fd67a2748d5e8309f4dfcf2ea1ac77ef6
ZJYH <1378286199@qq.com>
2020-09-18 16:32:39 +0800

    删除别的属性，只保留name合address

--- src/main/java/com/zh/springbootdemo/domain/User.java
+++ src/main/java/com/zh/springbootdemo/domain/User.java
@@ -2,9 +2,9 @@ package com.zh.springbootdemo.domain;
 
 public class User {
 
+    private String address;
     private String name;
-    private int age;
-    private String masterADD;
+   
 
     public String getName(){
         return this.name;
