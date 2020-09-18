7542e6f20c8344cb0b368c46c233bbe764d138dd
ZJYH <1378286199@qq.com>
2020-09-18 16:38:42 +0800

    删除address，添加setname

--- src/main/java/com/zh/springbootdemo/domain/User.java
+++ src/main/java/com/zh/springbootdemo/domain/User.java
@@ -2,11 +2,15 @@ package com.zh.springbootdemo.domain;
 
 public class User {
 
-    private String address;
+    
     private String name;
    
 
     public String getName(){
         return this.name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
     }
 }
