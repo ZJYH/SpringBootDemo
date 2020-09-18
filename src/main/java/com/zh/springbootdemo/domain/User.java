c59e6a476e7ec0859178534a79be882bd4c894db
zhanghao <zhanghao6@pec.com.cn>
2020-09-18 16:40:51 +0800

    添加address和account

--- src/main/java/com/zh/springbootdemo/domain/User.java
+++ src/main/java/com/zh/springbootdemo/domain/User.java
@@ -2,14 +2,17 @@ package com.zh.springbootdemo.domain;
 
 public class User {
 
-    
+
     private String name;
-   
+    private String account;
+    private String address;
+
+
 
     public String getName(){
         return this.name;
     }
-    
+
     public void setName(String name){
         this.name = name;
     }
