e16b8d226ff168fc00c5d43fca89bf832df41030
ZH <1378286199@qq.com>
2020-10-12 18:31:55 +0800

    增加一个getRename方法

--- src/main/java/com/zh/springbootdemo/domain/Dog.java
+++ src/main/java/com/zh/springbootdemo/domain/Dog.java
@@ -2,8 +2,15 @@ package com.zh.springbootdemo.domain;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+
 @Data
 public class Dog {
     private  String name;
     private  int age ;
+
+    public void getReName(){
+        int q= 12;
+        ArrayList<Object> objects = new ArrayList<>();
+    }
 }
